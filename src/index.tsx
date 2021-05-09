92911d84b384756e68e1e3a574a10a84adfe97d5
inokawa <48897392+inokawa@users.noreply.github.com>
2021-05-09 23:47:10 +0900

    Reflect devicePixelRatio to canvas

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
-import React, { memo, useCallback, useEffect, useRef, forwardRef } from "react";
+import React, {
+  memo,
+  useMemo,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  forwardRef,
+} from "react";
 import { render } from "react-dom";
 
 export type ImagifyProps = JSX.IntrinsicElements["canvas"] & {
-  type?: "canvas" | "svg";
+  type?: "canvas"; // | "svg";
 };
 
 const Mounter = ({
@@ -18,14 +26,38 @@ const Mounter = ({
   return <>{children}</>;
 };
 
-const Component = ({ type = "canvas", children, ...props }: ImagifyProps) => {
+const Component = ({
+  type = "canvas",
+  width: propWidth,
+  height: propHeight,
+  style: propStyle,
+  children,
+  ...props
+}: ImagifyProps) => {
   const svgRef = useRef(document.createElement("div"));
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  const dpr = window.devicePixelRatio ?? 1;
+  const width = Number(propWidth ?? 0) * dpr;
+  const height = Number(propHeight ?? 0) * dpr;
+
+  const style = useMemo<React.CSSProperties>(
+    () => ({
+      ...propStyle,
+      width: String(width / dpr) + "px",
+      height: String(height / dpr) + "px",
+    }),
+    [propStyle, width, height]
+  );
+
+  useLayoutEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) return;
+    ctx.scale(dpr, dpr);
+  }, []);
+
   useEffect(() => {
     (async () => {
-      const width = Number(props.width);
-      const height = Number(props.height);
       await new Promise<void>((resolve) => {
         render(
           <Mounter resolve={resolve}>
@@ -55,7 +87,15 @@ const Component = ({ type = "canvas", children, ...props }: ImagifyProps) => {
     })();
   }, [children]);
 
-  return <canvas ref={canvasRef} {...props} />;
+  return (
+    <canvas
+      {...props}
+      ref={canvasRef}
+      width={width}
+      height={height}
+      style={style}
+    />
+  );
 };
 
 export const Imagify = memo(Component);
