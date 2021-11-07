a6543a4834e6b54fb829892450105eb74e780102
inokawa <48897392+inokawa@users.noreply.github.com>
2021-11-07 20:57:13 +0900

    Refactor

---
+++ src/dom.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect } from "react";
+import { render } from "react-dom";
+
+const INLINE_BASE64 = /^data:image\/.*;base64,/i;
+const isInlineBase64Image = (src: string): boolean => INLINE_BASE64.test(src);
+
+const nodeToUrl = async (node: HTMLElement) => {
+  const svgStr = new XMLSerializer().serializeToString(node);
+  return "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svgStr);
+};
+
+const genImage = (url: string, width: number, height: number) => {
+  return new Promise<HTMLImageElement>((resolve, reject) => {
+    const img = new Image(width, height);
+    img.onload = (_) => {
+      resolve(img);
+    };
+    img.src = url;
+  });
+};
+
+const Mounter = ({
+  children,
+  resolve,
+}: {
+  children: React.ReactNode;
+  resolve: () => void;
+}) => {
+  useEffect(() => {
+    resolve();
+  }, [children]);
+  return <>{children}</>;
+};
+
+export const generateImageFromDOM = async (
+  children: React.ReactNode,
+  width: string | number | undefined,
+  height: string | number | undefined,
+  el: HTMLDivElement
+): Promise<HTMLImageElement> => {
+  await new Promise<void>((resolve) => {
+    render(
+      <Mounter resolve={resolve}>
+        <svg width={width} height={height}>
+          <foreignObject width={width} height={height}>
+            {children}
+          </foreignObject>
+        </svg>
+      </Mounter>,
+      el
+    );
+  });
+  await Promise.all(
+    Array.from(el.querySelectorAll("img")).map(async (img) => {
+      if (isInlineBase64Image(img.src)) return img;
+      return new Promise<string>((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = () => {
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            img.src = reader.result as string;
+            resolve(reader.result as string);
+          };
+          reader.readAsDataURL(xhr.response);
+        };
+        xhr.open("GET", img.src);
+        xhr.responseType = "blob";
+        xhr.send();
+      });
+    })
+  );
+
+  const svgUrl = await nodeToUrl(el.children[0] as HTMLElement);
+  const img = await genImage(svgUrl, Number(width), Number(height));
+  return img;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -7,26 +7,10 @@ import React, {
   useRef,
   forwardRef,
 } from "react";
-import { render } from "react-dom";
-
-const INLINE_BASE64 = /^data:image\/.*;base64,/i;
-const isInlineBase64Image = (src: string): boolean => INLINE_BASE64.test(src);
+import { generateImageFromDOM } from "./dom";
 
 export type ImagifyProps = JSX.IntrinsicElements["canvas"] & {
   type?: "canvas"; // | "svg";
-};
-
-const Mounter = ({
-  children,
-  resolve,
-}: {
-  children: React.ReactNode;
-  resolve: () => void;
-}) => {
-  useEffect(() => {
-    resolve();
-  }, [children]);
-  return <>{children}</>;
 };
 
 function mergeRefs<T extends any>(
@@ -39,21 +23,6 @@ function mergeRefs<T extends any>(
     });
   }, refs);
 }
-
-const nodeToUrl = async (node: HTMLElement) => {
-  const svgStr = new XMLSerializer().serializeToString(node);
-  return "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svgStr);
-};
-
-const genImage = (url: string, width: number, height: number) => {
-  return new Promise<HTMLImageElement>((resolve, reject) => {
-    const img = new Image(width, height);
-    img.onload = (_) => {
-      resolve(img);
-    };
-    img.src = url;
-  });
-};
 
 const Component = forwardRef<HTMLCanvasElement, ImagifyProps>(
   (
@@ -92,47 +61,11 @@ const Component = forwardRef<HTMLCanvasElement, ImagifyProps>(
 
     useEffect(() => {
       (async () => {
-        await new Promise<void>((resolve) => {
-          render(
-            <Mounter resolve={resolve}>
-              <svg width={widthProp} height={heightProp}>
-                <foreignObject width={widthProp} height={heightProp}>
-                  {children}
-                </foreignObject>
-              </svg>
-            </Mounter>,
-            svgRef.current
-          );
-        });
-        await Promise.all(
-          Array.from(svgRef.current.querySelectorAll("img")).map(
-            async (img) => {
-              if (isInlineBase64Image(img.src)) return img;
-              return new Promise<string>((resolve, reject) => {
-                const xhr = new XMLHttpRequest();
-                xhr.onload = () => {
-                  const reader = new FileReader();
-                  reader.onloadend = () => {
-                    img.src = reader.result as string;
-                    resolve(reader.result as string);
-                  };
-                  reader.readAsDataURL(xhr.response);
-                };
-                xhr.open("GET", img.src);
-                xhr.responseType = "blob";
-                xhr.send();
-              });
-            }
-          )
-        );
-
-        const svgUrl = await nodeToUrl(
-          svgRef.current.children[0] as HTMLElement
-        );
-        const img = await genImage(
-          svgUrl,
-          Number(widthProp),
-          Number(heightProp)
+        const img = await generateImageFromDOM(
+          children,
+          widthProp,
+          heightProp,
+          svgRef.current
         );
 
         const ctx = canvasRef.current?.getContext("2d");
