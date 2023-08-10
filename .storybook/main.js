68aaf8e1ca8f50e3421911c9edc7c84dd1260a31
inokawa <48897392+inokawa@users.noreply.github.com>
2023-08-11 00:05:30 +0900

    Upgrade everything

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
-module.exports = {
+export default {
   stories: [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-essentials"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
 };
---
+++ rollup.config.mjs
@@ -0,0 +1,50 @@
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
+import banner from "rollup-plugin-banner2";
+import pkg from "./package.json" assert { type: "json" };
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    typescript({
+      tsconfig: "./tsconfig.json",
+      outDir: ".",
+      declaration: true,
+      exclude: ["**/*.{spec,stories}.*"],
+    }),
+    getBabelOutputPlugin({
+      plugins: ["@babel/plugin-transform-react-pure-annotations"],
+    }),
+    terser({
+      ecma: 2015,
+      module: true,
+      compress: { passes: 5, unsafe: true, keep_fargs: false },
+      mangle: { properties: { regex: "^_" } },
+      format: {
+        // https://github.com/terser/terser/pull/550
+        // https://github.com/terser/terser/issues/968
+        comments: /@preserve|@lic|@cc_on|^\**!|__PURE__/i,
+        preserve_annotations: true,
+      },
+    }),
+    banner(() => '"use client";\n'),
+  ],
+  external: (id) =>
+    [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.devDependencies || {}),
+    ].some((d) => id.startsWith(d)),
+};
