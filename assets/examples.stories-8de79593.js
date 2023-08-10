0a0144e9e9bb91957d55995ad0d4aa1ef4d5b637
inokawa <inokawa@users.noreply.github.com>
2023-08-10 15:06:26 +0000

    deploy: 68aaf8e1ca8f50e3421911c9edc7c84dd1260a31

---
+++ assets/examples.stories-8de79593.js
@@ -0,0 +1,69 @@
+import{a as s,r as B}from"./index-60a5bb00.js";var U={exports:{}},v={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var A=s,F=Symbol.for("react.element"),H=Symbol.for("react.fragment"),V=Object.prototype.hasOwnProperty,$=A.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,J={key:!0,ref:!0,__self:!0,__source:!0};function D(e,t,r){var n,a={},o=null,d=null;r!==void 0&&(o=""+r),t.key!==void 0&&(o=""+t.key),t.ref!==void 0&&(d=t.ref);for(n in t)V.call(t,n)&&!J.hasOwnProperty(n)&&(a[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps,t)a[n]===void 0&&(a[n]=t[n]);return{$$typeof:F,type:e,key:o,ref:d,props:a,_owner:$.current}}v.Fragment=H;v.jsx=D;v.jsxs=D;U.exports=v;var M=U.exports;const i=M.jsx,c=M.jsxs,X=/^data:image\/.*;base64,/i,Y=e=>X.test(e),G=e=>{const t=new XMLSerializer().serializeToString(e);return"data:image/svg+xml;charset=utf-8,"+encodeURIComponent(t)},K=(e,t,r)=>new Promise((n,a)=>{const o=new Image(t,r);o.onload=d=>{n(o)},o.onerror=a,o.src=e}),Q=e=>new Promise((t,r)=>{e.complete&&t(),e.onload=n=>{t()},e.onerror=r}),Z=e=>new Promise((t,r)=>{const n=document.createElement("canvas"),a=n.getContext("2d");if(!a)return r();n.width=e.width,n.height=e.height,a.drawImage(e,0,0),t(n.toDataURL("image/png")),n.remove()}),ee=async(e,t,r)=>{await Promise.all(Array.from(e.querySelectorAll("img")).map(async o=>{Y(o.src)||(await Q(o),o.src=await Z(o))}));const n=G(e.children[0]);return await K(n,Number(t),Number(r))},S=({width:e,height:t,children:r,update:n})=>{const a=s.useState(()=>document.createElement("div"))[0];return s.useEffect(()=>{n(a,e,t)},[r,e,t]),B.createPortal(i("svg",{width:e,height:t,children:i("foreignObject",{width:e,height:t,children:r})}),a)};try{S.displayName="Mounter",S.__docgenInfo={description:"",displayName:"Mounter",props:{width:{defaultValue:null,description:"",name:"width",required:!0,type:{name:"string | number | undefined"}},height:{defaultValue:null,description:"",name:"height",required:!0,type:{name:"string | number | undefined"}},update:{defaultValue:null,description:"",name:"update",required:!0,type:{name:"(el: HTMLDivElement, width: string | number | undefined, height: string | number | undefined) => void"}}}}}catch{}const te=(...e)=>s.useCallback(t=>{e.forEach(r=>{!r||!t||(r.current=t)})},e),ne=s.forwardRef(({width:e,height:t,style:r,children:n,...a},o)=>{const d=s.useRef(null),u=window.devicePixelRatio??1,x=Number(e??0),y=Number(t??0),w=x*u,_=y*u,P=s.useMemo(()=>({...r,width:String(x)+"px",height:String(y)+"px"}),[r,x,y]);s.useLayoutEffect(()=>{var p;const m=(p=d.current)==null?void 0:p.getContext("2d");m&&m.scale(u,u)},[]);const z=s.useCallback(async(m,p,W)=>{var b;const I=(b=d.current)==null?void 0:b.getContext("2d");if(!I)return;const q=await ee(m,p,W);I.clearRect(0,0,w,_),I.drawImage(q,0,0)},[w,_]);return c("canvas",{...a,ref:te(o,d),width:w,height:_,style:P,children:[n,i(S,{width:e,height:t,update:z,children:n})]})}),l=s.memo(ne);try{l.displayName="Imagify",l.__docgenInfo={description:"",displayName:"Imagify",props:{}}}catch{}const re=""+new URL("Octocat-ff7ce735.png",import.meta.url).href,oe={title:"examples"},h=()=>c("div",{children:[i(l,{width:400,height:100,children:c("div",{style:{fontSize:"40px"},children:[i("em",{children:"I"})," like"," ",i("span",{style:{color:"white",textShadow:"0 0 2px blue"},children:"cheese"})]})}),c("div",{style:{fontSize:"40px"},children:[i("em",{children:"I"})," like"," ",i("span",{style:{color:"white",textShadow:"0 0 2px blue"},children:"cheese"})]})]}),g=()=>i("div",{children:i(l,{width:400,height:300,children:i("img",{src:re,width:400})})}),f=()=>{const[e,t]=s.useState(0),[r,n]=s.useState("textarea");return c("div",{children:[i(l,{width:200,height:400,children:c("div",{children:[i("div",{style:{background:e%2===0?"pink":"skyblue"},children:e}),i("div",{style:{wordBreak:"break-all",whiteSpace:"pre-wrap"},children:r})]})}),c("div",{children:[c("button",{onClick:()=>t(a=>a+1),children:["update count: ",e]}),i("textarea",{value:r,onChange:a=>n(a.target.value)})]})]})};var k,E,R;h.parameters={...h.parameters,docs:{...(k=h.parameters)==null?void 0:k.docs,source:{originalSource:`() => {
+  return <div>
+      <Imagify width={400} height={100}>
+        <div style={{
+        fontSize: "40px"
+      }}>
+          <em>I</em> like{" "}
+          <span style={{
+          color: "white",
+          textShadow: "0 0 2px blue"
+        }}>
+            cheese
+          </span>
+        </div>
+      </Imagify>
+      <div style={{
+      fontSize: "40px"
+    }}>
+        <em>I</em> like{" "}
+        <span style={{
+        color: "white",
+        textShadow: "0 0 2px blue"
+      }}>
+          cheese
+        </span>
+      </div>
+    </div>;
+}`,...(R=(E=h.parameters)==null?void 0:E.docs)==null?void 0:R.source}}};var C,O,N;g.parameters={...g.parameters,docs:{...(C=g.parameters)==null?void 0:C.docs,source:{originalSource:`() => {
+  return <div>
+      <Imagify width={400} height={300}>
+        <img src={Octocat} width={400} />
+      </Imagify>
+    </div>;
+}`,...(N=(O=g.parameters)==null?void 0:O.docs)==null?void 0:N.source}}};var L,j,T;f.parameters={...f.parameters,docs:{...(L=f.parameters)==null?void 0:L.docs,source:{originalSource:`() => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("textarea");
+  return <div>
+      <Imagify width={200} height={400}>
+        <div>
+          <div style={{
+          background: count % 2 === 0 ? "pink" : "skyblue"
+        }}>
+            {count}
+          </div>
+          <div style={{
+          wordBreak: "break-all",
+          whiteSpace: "pre-wrap"
+        }}>
+            {text}
+          </div>
+        </div>
+      </Imagify>
+      <div>
+        <button onClick={() => setCount(p => p + 1)}>
+          update count: {count}
+        </button>
+        <textarea value={text} onChange={e => setText(e.target.value)} />
+      </div>
+    </div>;
+}`,...(T=(j=f.parameters)==null?void 0:j.docs)==null?void 0:T.source}}};const ie=["HelloWorld","Image","WithState"];export{h as HelloWorld,g as Image,f as WithState,ie as __namedExportsOrder,oe as default};
+//# sourceMappingURL=examples.stories-8de79593.js.map
